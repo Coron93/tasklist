44bb77e60ee1d35d38b741c48974cc812d011eaa
shuhei.goda <shuhei.coron93@outlook.jp>
2022-03-12 19:05:06 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Tasks;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -31,23 +29,13 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
+     // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        Tasks t = new Tasks();
+        request.setAttribute("tasks", new Tasks());
 
-        t.setContent("task_1");
-
-        Timestamp crrentTime = new Timestamp(System.currentTimeMillis());
-        t.setCreated_at(crrentTime);
-        t.setUpdated_at(crrentTime);
-
-        em.persist(t);
-        em.getTransaction().commit();
-
-        response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-        em.close();
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
     }
 
 }
